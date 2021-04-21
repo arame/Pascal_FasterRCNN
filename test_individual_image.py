d0e747ddc96f2f829c10625dccb175284101c106
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2021-04-21 19:31:06 +0100

    Include .jpg files in the github repository

--- test_individual_image.py
+++ test_individual_image.py
@@ -4,12 +4,14 @@ import os
 import cv2
 from PIL import Image as PILImage
 import numpy as np
+import torch
 from torchvision import transforms as transforms
 import matplotlib.pyplot as plt
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
 ''' Code adapted from 
 https://colab.research.google.com/drive/1eAUjzV3nZXkUXi0spPg6zUHtJWaUSzFk?usp=sharing#scrollTo=8Dbx7HZFIKhz '''
-''' I got better results using the PILImage library.
+''' I got better results using the PILImage library to convert the image to a tensor.
     In order to annotate the image I had to use the cv2 library'''
 
 
@@ -19,28 +21,34 @@ def individual_image(fasterrcnn_model, t_image, img, path):
     print(prediction)
     boxes = prediction[0]["boxes"]
     labels = prediction[0]["labels"]
+    scores = torch.round(prediction[0]["scores"] * 10) / 10
 
     # this will help us create a different colour for each class
     COLOURS = np.random.uniform(0, 255, size=(Hyper.num_classes, 3))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    for i, box in enumerate(boxes):
-        label_index = labels[i]
-        text = Hyper.pascal_categories[label_index]
-        color = COLOURS[labels[i]]
-        cv2.rectangle(
-            img,
-            (int(box[0]), int(box[1])),
-            (int(box[2]), int(box[3])),
-            color, 2
-        )
-        cv2.putText(img, text, (int(box[0]), int(box[1]-5)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2,
-                    lineType=cv2.LINE_AA)
+    for j in range(2):
+        for i, box in enumerate(boxes):
+            label_index = labels[i]
+            score = str(scores[i].item())[0:3]
+            if j == 0:
+                text = f"{Hyper.pascal_categories[label_index]}"
+            else:
+                text = f"{Hyper.pascal_categories[label_index]} {score}"
+            color = COLOURS[labels[i]]
+            cv2.rectangle(
+                img,
+                (int(box[0]), int(box[1])),
+                (int(box[2]), int(box[3])),
+                color, 2
+            )
+            cv2.putText(img, text, (int(box[0]), int(box[1]-5)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1,
+                        lineType=cv2.LINE_AA)
 
-    file_bb = path.replace(".jpg", "_out.jpg")
-    print(f"output file {file_bb}")
-    cv2.imwrite(file_bb, img)
-    print("** Image saved, the end **")
+        file_bb = path.replace(".jpg", f"_out{j}.jpg")
+        print(f"output file {file_bb}")
+        cv2.imwrite(file_bb, img)
+    print("** Images saved, the end **")
 
 
 # this is same transform used in the dataloader code.
@@ -63,7 +71,9 @@ def get_image(file):
 
 
 if __name__ == "__main__":
-    t_image_, img_, path_ = get_image("cats_and_dogs.jpg")
+    # aeroplane_with_persons.jpg
+    # cats_and_dogs.jpg
+    t_image_, img_, path_ = get_image("aeroplane_with_persons.jpg")
     epoch = Hyper.total_epochs
     model = load_checkpoint(epoch)
     individual_image(model, t_image_, img_, path_)
